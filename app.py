cb80ab18b998c8a56eb53c0e128b5af8a6f6cf1f
Rotinuel <emmanuelisagenius@gmail.com>
2023-02-28 23:03:20 +0100

    database changes

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
 from flask import Flask, render_template, jsonify
-from database import engine
-from sqlalchemy import text
+from database import load_jobs_from_db
 
 app = Flask(__name__)
  
 
-def load_jobs_from_db():
-    with engine.connect() as conn:
-        result = conn.execute(text("select * from jobs"))
-    jobs = []
-    for row in result.all():
-        jobs.append(dict(row))
-    return jobs
+
 
 @app.route("/")
 def hello_imma():
@@ -20,9 +13,10 @@ def hello_imma():
                            jobs=jobs,
                            company_name='Juo')
 
-#@app.route("/api/jobs")
-#def list_jobs():
-#    return jsonify(jobs)
+@app.route("/api/jobs")
+def list_jobs():
+    jobs = load_jobs_from_db()
+    return jsonify(jobs)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug= True)
--- database.py
+++ database.py
@@ -10,3 +10,10 @@ engine = create_engine( db_connection_string,
 
 } )
 
+def load_jobs_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs"))
+    jobs = []
+    for row in result.all():
+        jobs.append(row)
+    return jobs
