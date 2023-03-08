4b4adb01c30546cd0874ad5408db409613262b2e
Rotinuel <emmanuelisagenius@gmail.com>
2023-03-08 20:18:32 +0100

    others

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request ,jsonify
 from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
@@ -23,6 +23,11 @@ def show_job(id):
     if not job:
         return "Not Found", 404
     return render_template('jobpage.html', job=job)
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_job(id):
+     data = request.form
+     return render_template('application.html', application=data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug= True)
