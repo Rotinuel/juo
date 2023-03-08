b55dffea7089caa12bb0aa4365d89c9bfeff8611
Rotinuel <emmanuelisagenius@gmail.com>
2023-03-08 14:43:04 +0100

    require

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ app = Flask(__name__)
 def hello_imma():
     jobs = load_jobs_from_db()
     return render_template( 'home.html', 
-                           jobs=jobs,
-                           company_name='Juo')
+                           jobs=jobs)
 
 @app.route("/api/jobs")
 def list_jobs():
@@ -21,6 +20,8 @@ def list_jobs():
 @app.route("/job/<id>")
 def show_job(id):
     job = load_job_from_db(id)
+    if not job:
+        return "Not Found", 404
     return render_template('jobpage.html', job=job)
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -1,5 +1,4 @@
 from flask import request
-from alembic import op
 from sqlalchemy import  create_engine, text
 import os
 
